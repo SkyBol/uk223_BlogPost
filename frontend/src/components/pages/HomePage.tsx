b09ed159a87f421901e20fee9c73f16f8ec3c14c
Orion30star <aryan.aravinth@bixbytessolutions.com>
2022-11-03 11:34:43 +0100

    Created header finished routing needs to be done!

--- frontend/src/components/pages/HomePage.tsx
+++ frontend/src/components/pages/HomePage.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import logo from '../../logo1.png';
+import ResponsiveAppBar from '../Header/ResponsiveAppBar';
+
 
 export default function HomePage() {
   return (
     <div>
+    <ResponsiveAppBar/>
       <img
         src={logo}
         style={{ filter: 'invert(100%)' }}
