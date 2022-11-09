6a32e7e139abf6f864dc6c2267f6fbf5b0a761ea
SkyBollin <sky.bollin@noseryoung.ch>
2022-11-09 10:20:44 +0100

    safety commit

--- backend/src/main/java/com/example/demo/domain/blogpost/BlogPostRepository.java
+++ backend/src/main/java/com/example/demo/domain/blogpost/BlogPostRepository.java
@@ -2,9 +2,10 @@ package com.example.demo.domain.blogpost;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public interface BlogPostRepository extends CrudRepository<BlogPost, UUID> {
-
+    List<BlogPost> findAllByGreaterThanEqualCreationTime(LocalDateTime creationTime);
 }
